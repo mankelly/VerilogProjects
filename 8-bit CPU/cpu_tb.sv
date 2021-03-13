d8d93c6e98d738b7b7f445a891dec7535acf9b60
Manuel Kelly <38482542+mankelly@users.noreply.github.com>
2021-03-13 15:06:36 -0800

    Update cpu_tb.sv

--- 8-bit CPU/cpu_tb.sv
+++ 8-bit CPU/cpu_tb.sv
@@ -13,11 +13,17 @@ initial begin
     reset = 1;
     #10 reset = 0;
     wr = 1;
-    A = 8'b00001000;
-    B = 8'b00000100;
-    opcode = 8'b00000000;
-    #10 opcode = 8'b00000010;
-    #10 opcode = 8'b00010100;
+    A = 8'b00001000; //8
+    B = 8'b00000100; //4
+    opcode = 8'b00000000; //add
+    #10 opcode = 8'b00000010; //sub
+    #10 opcode = 8'b00010100; //two's comp of A
+    #10 opcode = 8'b00001000; //and
+    #10 opcode = 8'b00001001; //or
+    #10 opcode = 8'b00001010; //xor
+    #10 opcode = 8'b00011011; //one's comp of A
+    #10 opcode = 8'b00001110; //rotate A right 
+    #10 opcode = 8'b00011110; //rotate A left
     #10 wr = 0;
 end
 always #5 clk = !clk;
